5749b5c0a54926bb9cc7c2cca8224e2f52f8c5ef
Kstanfor <bluffsummer@gmail.com>
2024-03-03 15:27:26 -0600

    K

--- CoopGrapplerProject/Assets/Scripts/FallToDeath.cs
+++ CoopGrapplerProject/Assets/Scripts/FallToDeath.cs
@@ -2,9 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class FallToDeath : MonoBehaviour
 {
+
     public TMP_Text output;
     public string textToDisplay;
 
