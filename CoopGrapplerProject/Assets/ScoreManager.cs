a521469ab7c240224f75de79050962e328c86ac4
Kstanfor <bluffsummer@gmail.com>
2024-03-03 18:17:59 -0600

    ScoreManager

---
+++ CoopGrapplerProject/Assets/ScoreManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static bool gameOver;
+    public static bool won;
+    public static int score;
+
+    public TMP_Text textbox;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        gameOver = false;
+        won = false;
+        score = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!gameOver)
+        {
+            textbox.text = "Score: " + score;
+        }
+
+        if (score >= 3) 
+        {
+            won = true;
+            gameOver = true;
+        }
+        if (gameOver)
+        {
+            if (won)
+            {
+                textbox.text = "You Win!\nPress R to Try Again!";
+            }
+            else
+            {
+                textbox.text = "You Lose!\nPress R to Try Again!";
+            }
+            if (Input.GetKeyDown(KeyCode.R)) 
+            { 
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name); 
+            }
+
+        }
+    }
+}
---
+++ CoopGrapplerProject/Assets/ScoreTriggerZone.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTriggerZone : MonoBehaviour
+{
+    bool active = true;
+
+    private void OnTriggerEnter2D (Collider2D collision) 
+    {
+        if (active && collision.gameObject.tag == "Player")
+        {
+            active = false;
+
+            ScoreManager.score++;
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
