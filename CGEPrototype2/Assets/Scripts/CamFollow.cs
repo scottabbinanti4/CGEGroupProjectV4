48e98a7f980153ea3d9e6819c1b2f35db68c5478
SneakkWow <logan.c.turney@outlook.com>
2024-04-07 19:30:41 -0500

    Added basic movement

---
+++ CGEPrototype2/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public GameObject player;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.position = new Vector3(
+            player.transform.position.x,
+            player.transform.position.y,
+            transform.position.z
+            );
+    }
+}
---
+++ CGEPrototype2/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    public float jumpForce = 10f;
+    public float jetpackForce = 20f;
+    public LayerMask groundLayer;
+    public Transform groundCheck;
+    public float groundCheckRadius = 0.2f;
+    public ParticleSystem jetpackParticles; // Reference to the jetpack particle system
+    private Rigidbody2D rb;
+    private bool isGrounded;
+    private float horizontalInput;
+    private bool isJetpackActive; // Flag to track if jetpack is currently active
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        if (groundCheck == null)
+        {
+            Debug.LogError("GroundCheck not assigned to the player controller!");
+        }
+    }
+
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+        }
+
+        if (Input.GetButton("Jump") && !isGrounded)
+        {
+            rb.AddForce(Vector2.up * jetpackForce, ForceMode2D.Force);
+            ActivateJetpackParticles(true); // Activate jetpack particles only once
+            isJetpackActive = true;
+        }
+        else if (isJetpackActive)
+        {
+            ActivateJetpackParticles(false); // Deactivate jetpack particles only once
+            isJetpackActive = false;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontalInput * moveSpeed, rb.velocity.y);
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+
+        if (horizontalInput > 0)
+        {
+            transform.localScale = new Vector3(1f, 1f, 1f);
+        }
+        else if (horizontalInput < 0)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+    }
+
+    // Method to activate or deactivate jetpack particles
+    void ActivateJetpackParticles(bool activate)
+    {
+        if (jetpackParticles != null)
+        {
+            if (activate)
+            {
+                if (!jetpackParticles.isPlaying)
+                {
+                    jetpackParticles.Play();
+                }
+            }
+            else
+            {
+                if (jetpackParticles.isPlaying)
+                {
+                    jetpackParticles.Stop();
+                }
+            }
+        }
+    }
+}
