db1d0ebf066ce8332e5176eaad2ed641eec2eeff
FoxW13 <foxweymouth@gmail.com>
2024-04-29 12:37:36 -0500

    folders and scripts

---
+++ CGEPrototype2/Assets/Scripts/GhostRunner.cs
@@ -0,0 +1,30 @@
+/*using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GhostRunner : MonoBehaviour
+{
+    [SerializeField] private Transform _recordTarget;
+    [SerializeField] private GameObject _ghostPrefab;
+    [SerializeField, Range(1, 10)] private int _captureEveryNFrames = 2;
+
+    private void Awake() => _system = new ReplaySystem(this);
+
+    private void OnEnable() => FinishLine.Crossed += OnFinishLineCrossed;
+    private void OnDisable() => FinishLine.Crossed -= OnFinishLineCrossed;
+
+    private void OnFinishLineCrossed(bool runStarting)
+    {
+        if (runStartgin)
+        {
+            _system.StartRun(_recordTarget, _captureEveryNFrames, _captureEveryNFrames);
+            _system.PlayRecording(RecordingType.Best, Instantiate(_ghostPrefab));
+        }
+        else
+        {
+            _system.FinishRun();
+            _system.StopReplay();
+        }
+    }
+}
+*/
